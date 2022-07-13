bc1c551de90490bc10e8cbba650b6e9c501e7df8
Rajesh Kumar <rajesh.kumar@infracloud.io>
2022-07-13 07:39:44 +0530

    Very fast port scanner, exits immediately. VERSION-3

--- src/port-scan/main.go
+++ src/port-scan/main.go
@@ -7,14 +7,16 @@ import (
 
 func main(){
    for i:=1; i<=1024;i++ {
-      address := fmt.Sprintf("scanme.nmap.org:%d", i)
-      conn,err := net.Dial("tcp",address)
-      if err != nil {
-         fmt.Printf("port %v is not open\n",i)
-         continue
-      } else {
-         conn.Close()
-         fmt.Printf("Connection was successful on port %v\n",i)
-      }
+      go func (j int) {
+         address := fmt.Sprintf("scanme.nmap.org:%d", j)
+         conn,err := net.Dial("tcp",address)
+         if err != nil {
+            fmt.Printf("port %v is not open\n",j)
+            return
+         } else {
+            conn.Close()
+            fmt.Printf("Connection was successful on port %v\n",j)
+         }
+      }(i)
    }
 }
