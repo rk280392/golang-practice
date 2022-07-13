dacda6d69fc93f2b3f0be9859ba6df9d78518ca3
Rajesh Kumar <rajesh.kumar@infracloud.io>
2022-07-13 07:51:18 +0530

    Updated comments and README for VERSION-4

--- src/port-scan/main.go
+++ src/port-scan/main.go
@@ -7,11 +7,11 @@ import (
 )
 
 func main(){
-   var wg sync.WaitGroup //WaitGroup is a struct type and can be created as this 
+   var wg sync.WaitGroup //WaitGroup is a struct type and acts as a synchronized counter 
    for i:=1; i<=1024;i++ {
-      wg.Add(1)
+      wg.Add(1) // increment counter via wg.Add(1) each time a goroutine is created to scan a port
       go func(j int) {
-         defer wg.Done()
+         defer wg.Done() // decrements the counter whenever one unit of work has been performed 
          address := fmt.Sprintf("scanme.nmap.org:%d", j)
          conn,err := net.Dial("tcp",address)
          if err != nil {
@@ -23,5 +23,6 @@ func main(){
          }
       }(i)
    }
+   // main() function calls wg.Wait(), which blocks until all the work has been done and your counter has returned to zero
    wg.Wait()
 }
