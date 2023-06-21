879c5f4b0cb171d1523ed3652d9aaee4d596d0e2
Rajesh Kumar <srk280392@gmail.com>
2023-06-21 09:58:09 +0530

    fixed matrixDiagnolDifference

--- hackerrank/matrixDiagnolDifference/main.go
+++ hackerrank/matrixDiagnolDifference/main.go
@@ -34,56 +34,9 @@ func diagonalDifference(arr [][]int32) int32 {
 }
 
 func main() {
-	reader := bufio.NewReaderSize(os.Stdin, 16*1024*1024)
-
-	stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
-	checkError(err)
-
-	defer stdout.Close()
-
-	writer := bufio.NewWriterSize(stdout, 16*1024*1024)
-
-	nTemp, err := strconv.ParseInt(strings.TrimSpace(readLine(reader)), 10, 64)
-	checkError(err)
-	n := int32(nTemp)
-
 	var arr [][]int32
-	for i := 0; i < int(n); i++ {
-		arrRowTemp := strings.Split(strings.TrimRight(readLine(reader), " \t\r\n"), " ")
-
-		var arrRow []int32
-		for _, arrRowItem := range arrRowTemp {
-			arrItemTemp, err := strconv.ParseInt(arrRowItem, 10, 64)
-			checkError(err)
-			arrItem := int32(arrItemTemp)
-			arrRow = append(arrRow, arrItem)
-		}
-
-		if len(arrRow) != int(n) {
-			panic("Bad input")
-		}
-
-		arr = append(arr, arrRow)
-	}
-
 	result := diagonalDifference(arr)
 
-	fmt.Fprintf(writer, "%d\n", result)
+	fmt.Println(result)
 
-	writer.Flush()
-}
-
-func readLine(reader *bufio.Reader) string {
-	str, _, err := reader.ReadLine()
-	if err == io.EOF {
-		return ""
-	}
-
-	return strings.TrimRight(string(str), "\r\n")
-}
-
-func checkError(err error) {
-	if err != nil {
-		panic(err)
-	}
 }
