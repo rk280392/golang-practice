5de5108ffb52fa824f7906a09b60bf4ba4864ec6
Rajesh Kumar <srk280392@gmail.com>
2023-06-23 17:33:19 +0530

    Solved Birthday

--- hackerrank/birthday/main.go
+++ hackerrank/birthday/main.go
@@ -8,14 +8,21 @@ func birthday(s []int32, d int32, m int32) int32 {
 	var count int32
 	var n int32 = int32(len(s))
 
-	// Iterate through chocolate bar
+	// Iterate through chocolate bar till i < length of bar - length of pair needed.
+
 	for i := int32(0); i < n-m; i++ {
 		var sum int32
 
+		// Iterate to get the next number. Loop should run for i + length of pair needed.
+
 		for j := i; j < i+m; j++ {
+			fmt.Println("i=", i)
+			fmt.Println("j=", j)
+
+			// calculate the sum
 			sum += s[j]
 		}
-
+		fmt.Println(sum)
 		if sum == d {
 			count++
 		}
